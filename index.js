90c17ca26f18e4767ca627163c9645aa307bac83
Anderson Nascimento <andersonsilva.nascimento98@gmail.com>
2022-01-16 23:13:57 -0300

    parte html, css e responsiva  completa e menu feito em javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+
+addEventListener('click', (e) => {
+    let el = e.target
+    if (el.classList == 'modal') {        
+
+        let nav = document.getElementById('nav')
+        nav.classList.toggle('active')
+      
+    } 
+    
+})
+
+
+
+
+
