a6c86cd9f92d9f0041268d40bd8b255540f8784e
Anderson Nascimento <andersonsilva.nascimento98@gmail.com>
2022-02-04 00:02:40 -0300

    ajustando tamanhos de fontes e posicionamentos

--- assents/src/index.js
+++ assents/src/index.js
@@ -1,10 +1,11 @@
 
 addEventListener('click', (e) => {
     let el = e.target
+    console.log(el.classList)
     if (el.classList == 'modal') {        
 
         let nav = document.getElementById('nav')
-        nav.classList.toggle('active')
+        nav.classList.toggle('active')   // toggle cria uma classe no elemento caso ela não exista, caso exista ele remove
       
     } 
     
